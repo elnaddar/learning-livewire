bb5168b07455bfc68a29324af9ef6ea753f6c280
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-11 13:10:14 +0300

    applyed tailwind style to the form component

--- resources/views/livewire/my-form.blade.php
+++ resources/views/livewire/my-form.blade.php
@@ -1,14 +1,19 @@
-<div>
-    <h1>Form</h1>
-    <form wire:submit='createNewUser' action="">
-        <input wire:model='name' type="type" placeholder="name..">
-        <input wire:model='email' type="email" placeholder="email..">
-        <input wire:model='password' type="password" placeholder="password..">
-        <input type="submit" value="submit">
+<div class="p-5">
+    <h1
+        class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Form</h1>
+    <form class="p-5" wire:submit='createNewUser' action="">
+        <input class="block rounded border border-gray-300 px-3 py-1 mb-1" wire:model='name' type="type"
+            placeholder="name..">
+        <input class="block rounded border border-gray-300 px-3 py-1 mb-1" wire:model='email' type="email"
+            placeholder="email..">
+        <input class="block rounded border border-gray-300 px-3 py-1 mb-1" wire:model='password' type="password"
+            placeholder="password..">
+        <input class="block rounded px-3 py-1 bg-gray-400 text-white"type="submit" value="submit">
     </form>
     <hr>
-    <h2>All Users: {{ count($users) }}</h2>
-    <ol>
+    <h2 class="text-4xl font-extrabold dark:text-white">All Users: {{ count($users) }}</h2>
+    <ol class="list-decimal">
         @foreach ($users as $user)
             <li>
                 <h3>{{ $user->name }}</h3>
