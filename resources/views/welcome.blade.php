2ab84257387501c5e3e4f0764de998a445e6fa2f
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-11 13:01:18 +0300

    initialized tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <title>Laravel</title>
 
+    @vite('resources/css/app.css')
+</head>
 
-    </head>
-    <body class="antialiased">
-        {{-- @livewire('clicker') --}}
-        <livewire:clicker/>
-        <hr>
-        @livewire('my-form')
-    </body>
+<body class="antialiased">
+    {{-- @livewire('clicker') --}}
+    <livewire:clicker />
+    <hr>
+    @livewire('my-form')
+</body>
+
 </html>
