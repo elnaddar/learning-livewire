93af0d1b237fce551d38cd79f8e5e802e6a4f67d
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-11 12:15:08 +0300

    made the clicker to be counter

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -6,9 +6,18 @@ use Livewire\Component;
 
 class Clicker extends Component
 {
-    public function handleClick(){
-        dump("clicked");
+    public $count = 0;
+
+    public function increment()
+    {
+        $this->count++;
     }
+
+    public function decrement()
+    {
+        $this->count--;
+    }
+
     public function render()
     {
         return view('livewire.clicker');
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,5 +1,9 @@
 <div>
-    <button wire:click='handleClick'>
-        Click me
+    <h2>Counter: {{ $count }}</h2>
+    <button wire:click='increment'>
+        Count ++
+    </button>
+    <button wire:click='decrement'>
+        Count --
     </button>
 </div>
