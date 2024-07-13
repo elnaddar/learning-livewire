91715d28d53831477c8b0dd8094b6cd676c32adf
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-13 18:26:41 +0300

    added image upload feature

--- app/Livewire/MyForm.php
+++ app/Livewire/MyForm.php
@@ -4,21 +4,26 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Attributes\Rule;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 use Livewire\WithPagination;
 
 class MyForm extends Component
 {
     use WithPagination;
-    
+    use WithFileUploads;
     #[Rule("required|min:2|max:50")]
     public $name;
     #[Rule("required|email|unique:users")]
     public $email;
     #[Rule("required|min:5")]
     public $password;
-    public function createNewUser(){
-        $this -> validate();
+    #[Validate("nullable|sometimes|image|max:2046")]
+    public $image;
+    public function createNewUser()
+    {
+        $this->validate();
 
         // if you will not use #[Rule()]
         // you can use
@@ -29,18 +34,19 @@ class MyForm extends Component
         // ]);
 
         User::create([
-            "name"=> $this->name,
-            "email"=> $this->email,
-            "password"=> bcrypt($this->password),
+            "name" => $this->name,
+            "email" => $this->email,
+            "password" => bcrypt($this->password),
+            "image" => $this->image->store("uploaded/profile", "public")
         ]);
 
-        $this -> reset();
+        $this->reset();
         // $this -> reset(["password"]); // if you want to reset password only
     }
 
     public function render()
     {
         $users = User::paginate(3);
-        return view('livewire.my-form', ['users'=> $users]);
+        return view('livewire.my-form', ['users' => $users]);
     }
 }
--- resources/views/livewire/my-form.blade.php
+++ resources/views/livewire/my-form.blade.php
@@ -3,24 +3,30 @@
         class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         Form</h1>
     <form class="p-5" wire:submit='createNewUser' action="">
-        <input class="block rounded border border-gray-300 px-3 py-1 mb-1" wire:model='name' type="type"
+        <input class="block px-3 py-1 mb-1 border border-gray-300 rounded" wire:model='name' type="type"
             placeholder="name..">
         @error('name')
-            <div class="text-red-500 text-xs">{{ $message }}</div>
+            <div class="text-xs text-red-500">{{ $message }}</div>
         @enderror
-        <input class="block rounded border border-gray-300 px-3 py-1 mb-1" wire:model='email' type="email"
+        <input class="block px-3 py-1 mb-1 border border-gray-300 rounded" wire:model='email' type="email"
             placeholder="email..">
         @error('email')
-            <div class="text-red-500 text-xs">{{ $message }}</div>
+            <div class="text-xs text-red-500">{{ $message }}</div>
         @enderror
 
-        <input class="block rounded border border-gray-300 px-3 py-1 mb-1" wire:model='password' type="password"
+        <input class="block px-3 py-1 mb-1 border border-gray-300 rounded" wire:model='password' type="password"
             placeholder="password..">
         @error('password')
-            <div class="text-red-500 text-xs">{{ $message }}</div>
+            <div class="text-xs text-red-500">{{ $message }}</div>
         @enderror
 
-        <input class="block rounded px-3 py-1 bg-gray-400 text-white hover:bg-gray-600"type="submit" value="submit">
+        <input wire:model='image' accept="image/png image/jpeg" type="file"
+            class="block w-full text-sm bg-gray-900 rounded ring-1 ring-inset ring-gray-300">
+        @error('image')
+            <div class="text-xs text-red-500">{{ $message }}</div>
+        @enderror
+
+        <input class="block px-3 py-1 text-white bg-gray-400 rounded hover:bg-gray-600"type="submit" value="submit">
     </form>
     <hr>
     <h2 class="text-4xl font-extrabold dark:text-white">All Users: {{ count($users) }}</h2>
