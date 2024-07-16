a8f3e45c3fb34843d80487801f872cfb936d7aa4
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-16 21:21:57 +0300

    created register component

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,10 +2,37 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Register extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+    public $password_confirmation;
+
+    protected $rules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8|confirmed',
+    ];
+
+    public function register()
+    {
+        $this->validate();
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        auth()->login($user);
+
+        return redirect()->route('home');
+    }
     public function render()
     {
         return view('livewire.auth.register');
