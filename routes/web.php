ea57c353e2a8a95c66efadc654d130797784bbd8
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-16 21:31:51 +0300

    put routes to correct middlewares.

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,19 @@ use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home')->middleware('auth');
-Route::get('/register', Register::class)->name('register');
-Route::get('/login', Login::class)->name('login');
-Route::get('/logout', function () {
-    Auth::logout();
-    return redirect()->route('login');
-})->name('logout');
+Route::middleware('guest')->group(function () {
+    Route::get('/register', Register::class)->name('register');
+    Route::get('/login', Login::class)->name('login');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
+    Route::get('/logout', function () {
+        Auth::logout();
+        return redirect()->route('login');
+    })->name('logout');
+});
+
